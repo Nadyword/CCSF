5964458a6a6d3e7c1abbff2788a94134a7fb912a
Nadyword <83626464+Nadyword@users.noreply.github.com>
2026-06-20 18:33:44 -0400

    Update next.config.mjs

--- FrontEnd/next.config.mjs
+++ FrontEnd/next.config.mjs
@@ -4,14 +4,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*.public.blob.vercel-storage.com',
-        pathname: '/**',
-      },
-    ],
+    unoptimized: true,
   },
 }
 
